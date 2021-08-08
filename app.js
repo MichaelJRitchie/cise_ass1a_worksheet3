b770384f19a85ebda3544ab3d828657ab2dd10fb
MichaelJRitchie <dnw7874@autuni.ac.nz>
2021-08-09 10:07:47 +1200

    Final heroku deployment

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // app.js
-
+const path = require('path');
 const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
@@ -16,12 +16,29 @@ connectDB();
 app.use(cors({ origin: true, credentials: true }));
 
 // Init Middleware
-app.use(express.json({ extended: false }));
+//app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+//app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
 app.use('/api/books', books);
+
+if(process.env.NODE_ENV === "production")
+{
+    app.use(express.static(path.join(__dirname, '/cise-ass1a-worksheet3/build')))
+
+    app.get('*', (req, res) => 
+    {
+        res.sendFile(path.join(__dirname, 'cise-ass1a-worksheet3', 'build', 'index.html'))
+
+    })
+} else
+{
+    app.get('/', (req, res) => 
+    {
+        res.send("API is running");
+    })
+}
 
 const port = process.env.PORT || 8082;
 
